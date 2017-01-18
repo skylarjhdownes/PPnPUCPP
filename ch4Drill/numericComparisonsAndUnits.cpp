8ea0b2e3598cf6a413aee93626ca4cbf9b56c660
U-ATOMIC\downes <downes@downes1.dmea.osd.mil>
2017-01-17 18:07:53 -0800

    finished ch4 problem 5

--- ch4Drill/numericComparisonsAndUnits.cpp
+++ ch4Drill/numericComparisonsAndUnits.cpp
@@ -2,9 +2,22 @@
 
 int main()
 {
-  int num1, num2;
+  double num1, num2;
   while(cin >> num1 && cin >> num2){
-    cout << "The smallest value is: " << num2;
+    
+    if (num1 < num2) {
+      cout << "The smallest value is: " << num1 << ". \nThe largest value is: " << num2 << ".\n";
+    }
+    else if (num1 > num2) {
+      cout << "The smallest value is: " << num2 << ". \nThe largest value is: " << num1 << ".\n";
+    }
+    else {
+      cout << "The numbers are equal. \n";
+    }
+    
+    if (abs(num1-num2) < .01 && num1 != num2){
+      cout << "The numbers are almost equal.  \n";
+    }
   }
    
   return 0;
