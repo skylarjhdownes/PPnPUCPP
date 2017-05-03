c52793d63fa2c053e937122aeb384c5981184eea
Skylar Downes <skylarjhdownes@gmail.com>
2017-05-03 15:23:53 -0700

    cleanup

--- ch5Drill/debuggingDrill.cpp
+++ ch5Drill/debuggingDrill.cpp
@@ -1,9 +1,8 @@
-#include "std_lib_facilities.h"
+#include "../std_lib_facilities.h"
 
-#include "std_lib_facilities.h"
 int main()
 try {
-  
+
   // 1
   // Cout << "Success!\n";
   cout << "1: Success!\n";
@@ -15,28 +14,28 @@ try {
   // 3
   //cout << "Success" << !\n"
   cout << "3: Success" << "!\n";
-  
+
   // 4
   //cout << success << '\n';
   cout << "4: Success!" << '\n';
-  
+
   // 5
   //string res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
   int res = 7;
   vector<int> v(10);
   v[5] = res;
   cout << "5: Success!\n";
-  
+
   // 6
   //vector<int> v(10); v(5) = 7; if (v(5)!=7) cout << "Success!\n";
   vector<int> v2(10);
   v2[5] = 7;
   if (v2[5] == 7) cout << "6: Success!\n";
-  
+
   // 7
   //if (cond) cout << "Success!\n"; else cout << "Fail!\n";
   bool cond = true;
-  if (cond) cout << "7: Success!\n"; 
+  if (cond) cout << "7: Success!\n";
   else cout << "Fail!\n";
 
   // 8
@@ -44,39 +43,39 @@ try {
   bool c = true;
   if (c) cout << "8: Success!\n";
   else cout << "Fail!\n";
-  
+
   // 9
   //string s = "ape"; boo c = "fool"<s; if (c) cout << "Success!\n";
   string s = "ape";
   bool c2 = "fool" > s;
   if (c2) cout << "9: Success!\n";
-  
+
   // 10
   //string s = "ape"; if (s=="fool") cout << "Success!\n";
   string s2 = "ape";
   if (s2 != "fool") cout << "10: Success!\n";
-  
+
   // 11
   //string s = "ape"; if (s=="fool") cout < "Success!\n";
   string s3 = "ape";
   if (s3 != "fool") cout << "11: Success!\n";
-  
+
   // 12
   //string s = "ape"; if (s+"fool") cout < "Success!\n";
   string s4 = "ape";
   if (s4 != "fool") cout << "12: Success!\n";
-  
+
   // 13
   //vector<char> v(5); for (int i=0; 0<v.size(); ++i) ; cout << "Success!\n";
   vector<char> v3(5);
   for (std::size_t i = 0; i < v3.size(); ++i) ;
   cout << "13: Success!\n";
-  
+
   // 14
   //vector<char> v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
   vector<char> v4(5);
   for (std::size_t i = 0; i <= v4.size(); ++i) ; cout << "14: Success!\n";
-  
+
   // 15
   //string s = "Success!\n"; for (int i=0; i<6; ++i) cout << s[i];
   string s5 = "15: Success!\n";
@@ -84,29 +83,29 @@ try {
 
   // 16
   //if (true) then cout << "Success!\n"; else cout << "Fail!\n";
-  if (true) cout << "16: Success!\n"; 
+  if (true) cout << "16: Success!\n";
   else cout << "Fail!\n";
-  
+
   // 17
   //int x = 2000; char c = x; if (c==2000) cout << "Success!\n";
   int x = 2000;
   char c3 = 'x';
   if (x == 2000) cout << "17: Success!\n";
-  
+
   // 18
   //string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
   string s6 = "18: Success!\n";
   for (int i = 0; i < 13; ++i) cout << s6[i];
-  
+
   // 19
   //vector v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
   vector<int> v5(5);
   for (std::size_t i = 0; i <= v.size(); ++i) ;
-  cout << "19: Success!\n"; 
+  cout << "19: Success!\n";
 
   // 20
   //int i=0; int j = 9; while (i<10) ++j; if (j<i) cout << "Success!\n";
-  int i = 0; 
+  int i = 0;
   int j = 9;
   while (i < 10) ++i;
   if (j < i) cout << "20: Success!\n";
